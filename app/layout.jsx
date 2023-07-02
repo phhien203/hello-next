9fdaf155e56058a5dc74cc423e864506fa2ef7c1
Pham Huu Hien <phhien203@gmail.com>
2023-07-02 16:59:32 +0700

    add footer timestamp

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,15 @@
 import './styles.css'
 
 export default function RootLayout({ children }) {
+  console.log('page rendered')
+  const date = new Date().toLocaleString()
+
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <footer>Page rendered on {date}</footer>
+      </body>
     </html>
   )
 }
