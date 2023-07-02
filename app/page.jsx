c8e0ecea8cdfe1b7b108c28a1b42b62b1f2d0947
Pham Huu Hien <phhien203@gmail.com>
2023-07-02 17:51:36 +0700

    add hit counter

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,14 @@
+import { readFile, writeFile } from '../helpers/file-helpers'
+
 export default function HomePage() {
-  return <h1>Hello NextJS</h1>
+  const db = JSON.parse(readFile('/db.json'))
+  db.hits = db.hits += 1
+  writeFile('/db.json', JSON.stringify(db))
+
+  return (
+    <>
+      <h1>Hello NextJS</h1>
+      <p>You are visitor number {db.hits}</p>
+    </>
+  )
 }
